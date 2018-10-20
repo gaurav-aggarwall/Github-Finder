def1d5b0caf10f86d094de7ed3092ba3be8bab3e
gaurav1209 <aggarwal.gaurav10@gmail.com>
2018-10-20 14:01:24 +0530

    Github Comp and Search

---
+++ github-finder/src/Components/Profile.js
@@ -0,0 +1,13 @@
+import React , { Component } from 'react';
+
+class Profile extends Component {
+  render(){
+    return(
+      <div>
+        Hello
+      </div>
+    );
+  }
+}
+
+export default Profile;
--- github-finder/src/Components/Viewer.js
+++ github-finder/src/Components/Viewer.js
@@ -1,10 +1,55 @@
 import React , { Component } from 'react';
+import Profile from './Profile';
+import Search from './Search';
 
+const API = 'https://api.github.com/users/';
 class Viewer extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      username: 'gaurav1209',
+      name: '',
+      followers: '',
+      following: '',
+      avatar: '',
+      location: '',
+      repos: '',
+      notFound: ''
+    };
+  }
+
+  getProfile(username){
+    let loginURL = API + username;
+
+    fetch(loginURL)
+    .then((res) => res.json())
+    .then((data) => {
+      this.setState({
+        username: data.login,
+        name: data.name,
+        followers: data.followers,
+        following: data.following,
+        avatar: data.avatar_url,
+        location: data.location,
+        repos: data.public_repos,
+        notFound: data.message
+      })
+    })
+    .catch(err => console.log("There is some Problem, Please try later.") )
+  }
+
+  componentDidMount(){
+    this.getProfile(this.state.username);
+  }
+
   render(){
     return(
       <div>
-        Hello
+        <section id='card'>
+          <Search searchProfile={this.getProfile.bind(this)}/>
+          <Profile userData={this.state}/>
+        </section>
       </div>
     );
   }
